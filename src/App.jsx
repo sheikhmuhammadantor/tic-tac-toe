d68f32df2816e1dddfc764c9449eeebe03d66979
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-05-07 09:36:15 +0600

    Initialize Tic Tac Toe application with React and Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-center my-4 font-bold">Tic Tac Toe</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
